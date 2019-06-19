81f9f28f8e66ed078928eddb736915e299b4e6d3
David Osama <david_u_riad@hotmail.com>
2019-06-19 21:56:42 +0200

    added a single API for caption and tags

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from PIL import Image
 import os
 import model_caption
 import model_detection
 import base64
+import process_models_output
 
 app = Flask(__name__)
 
@@ -12,9 +13,46 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
+# REST API send image file with key 'image' and returns BOTH caption and tags
+@app.route("/api/image/", methods=['POST'])
+def detect_tag_image():
+    print('new tag & caption request received')
+    image_string = request.json['image']
+    print('decoding image file')
+    image = base64.b64decode(image_string)
+    filename = "img"
+    format_txt = ".jpg"
+    path='uploaded_images/'+filename + format_txt
+    imgFile = open(path, 'wb')
+    imgFile.write(image)
+    print('saving image on the server is done')
+
+    # get the tags
+    objects = model_detection.start(os.path.join('uploaded_images', filename+format_txt))
+    objects = process_models_output.ProcessTags(objects)
+    print('objects: '+objects)
+
+    # get the caption
+    caption = model_caption.generate_caption(os.path.join('uploaded_images', filename+format_txt))
+    caption = process_models_output.ProcessCaption(caption)
+    print('caption: '+caption)
+
+    # delete the image file
+    os.remove('uploaded_images/'+filename+format_txt)
+
+    data = {
+        'caption': caption,
+        'tags': objects
+    }
+
+    return jsonify(data)
+
+
+# REST API send image file with key 'image' and returns the tags
 @app.route("/api/detection", methods=['POST'])
 def detection():
-    print('new object detection request received')
+    print('new tag request received')
     image_string = request.json['image']
     print('decoding image file')
     image = base64.b64decode(image_string)
@@ -26,11 +64,13 @@ def detection():
     print('saving image temporarily is done')
 
     objects = model_detection.start(os.path.join('uploaded_images', filename+format_txt))
-    print('objects: '+objects)
-    
+    returnobjects = process_models_output.ProcessTags(objects)
+    print('objects: '+returnobjects)
+
     return objects
 
-# REST API send image file with tag 'image'
+
+# REST API send image file with key 'image' and returns the caption
 @app.route("/api/caption", methods=['POST'])
 def caption():
     print('new image caption request received')
@@ -44,28 +84,23 @@ def caption():
     print('saving image temporarily is done')
 
     caption = model_caption.generate_caption(os.path.join('uploaded_images', filename+format_txt))
-    captionWords = caption.split()
-    finalCaption = []
-    if(captionWords[-1]=="eeee"):
-        for i in range(0,len(captionWords)-1):
-            finalCaption.append(captionWords[i])
-
-    returncap = " ".join(finalCaption)
+    returncap = process_models_output.ProcessCaption(caption)
     print('caption: '+returncap)
     
     return returncap
 
+
 @app.route("/testdetection")
 def testdetection():
     objects = model_detection.start("images/img.jpg")
-    
+    objects = process_models_output.ProcessTags(objects)
     return render_template('result.html', caption=objects, image="img.jpg")
 
 
 @app.route("/testcaption")
 def testcaption():
     caption = model_caption.generate_caption("images/img.jpg")
-    
+    caption = process_models_output.ProcessCaption(caption)
     return render_template('result.html', caption=caption, image="img.jpg")
 
 
---
+++ process_models_output.py
@@ -0,0 +1,29 @@
+
+
+def ProcessCaption(caption):
+    captionWords = caption.split()
+    finalCaption = []
+    if(captionWords[-1]=="eeee"):
+        for i in range(0,len(captionWords)-1):
+            finalCaption.append(captionWords[i])
+
+    returncap = " ".join(finalCaption)
+    return returncap
+
+def ProcessTags(tags):
+    tagsArray = tags.split()
+    tagsDict = {i:tagsArray.count(i) for i in tagsArray}
+    finalTag = ""
+
+    for key, value in tagsDict.items():
+        if(value > 1):
+            lastLetter = key[-1:]
+            last2Letters = key[-2:]
+            if(lastLetter == "s" or lastLetter == "x" or lastLetter == "z" or last2Letters == "sh" or last2Letters == "ch"):
+                finalTag += str(value)+" "+key+"es "
+            else:
+                finalTag += str(value)+" "+key+"s "
+        else:
+            finalTag += key
+
+    return finalTag
